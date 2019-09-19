0329b0803e817a6acea08d17fd2ebfdd277799bc
samialperen <sami.akgun@ieee.metu.edu.tr>
2019-09-19 19:28:04 -0400

    Removed build directory

--- assignment1/ros_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/saakgun/Projects/Embodied-Intelligence/assignment1/ros_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/saakgun/Projects/Embodied-Intelligence/assignment1/ros_ws/devel;/opt/ros/melodic".split(';') if "/home/saakgun/Projects/Embodied-Intelligence/assignment1/ros_ws/devel;/opt/ros/melodic" != "" else []
