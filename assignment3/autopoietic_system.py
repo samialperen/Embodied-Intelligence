c705a8b25ff3b3d838d61aafeafa364e4622a29d
samialperen <sami.akgun@ieee.metu.edu.tr>
2019-10-10 17:19:17 -0400

    Autopoetic System simulation assignment is started.

---
+++ assignment3/autopoietic_system.py
@@ -0,0 +1,12 @@
+##### Author: Sami Alperen Akgun
+#### University of Waterloo, ECE Department
+
+# Python Turtle Graphics Module 
+import turtle
+
+## Adjust Display
+disp = turtle.Screen()
+
+autopoetic_entity = turtle.Turtle()
+
+input("Press Any Key to Execute the Program")
