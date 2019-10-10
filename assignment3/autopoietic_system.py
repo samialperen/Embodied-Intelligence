e988914c11a92949ff4fd3d2c3e83ef25afb4d14
samialperen <sami.akgun@ieee.metu.edu.tr>
2019-10-10 18:01:37 -0400

    Work in progress

--- assignment3/autopoietic_system.py
+++ assignment3/autopoietic_system.py
@@ -6,7 +6,36 @@ import turtle
 
 ## Adjust Display
 disp = turtle.Screen()
+disp.bgcolor("gold")
 
-autopoetic_entity = turtle.Turtle()
+## Draw rectangular boundary for simulation area
+pen = turtle.Turtle()
+pen.penup()
+pen.setposition(-300,-300)
+pen.pensize(3)
+pen.pendown()
+for i in range(4):
+    pen.forward(600)
+    pen.left(90)
+pen.hideturtle()
+
+
+## Spawn catalyst
+catalyst = turtle.Turtle()
+catalyst.color("blue")
+catalyst.shape("turtle")
+catalyst.penup()
+
+def go_straight():
+    catalyst.forward(2)
+
+turtle.listen()
+turtle.onkey(go_straight,"Up")
+
+
+
+
+
+
 
 input("Press Any Key to Execute the Program")
