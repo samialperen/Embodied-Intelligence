da42959ec8e211edcff8c96028b5876772327e1e
samialperen <sami.akgun@ieee.metu.edu.tr>
2019-10-19 00:54:29 -0400

    Production phase is almost finished

--- assignment3/autopoietic_system.py
+++ assignment3/autopoietic_system.py
@@ -137,11 +137,17 @@ for y in range( (-area_length//2)+ (grid_length//2), (area_length//2) + (grid_le
 #####################################################
 
 ##################################################### 
-def check_substrate_neighbors(n,current_grid,substrates,substrate_number,catalyst_index,grid_shift):
+def check_substrate_neighbors(current_grid,substrates,substrate_number,catalyst_index,grid_shift,n_holes,n_link,n_bounded_link):
     # This function checks whether current_grid's neighbors contain substrate and returns
     # index of neighbor grids that contain substrates  
-
+    n = []
     for i in range(substrate_number):
+        if i in n_holes:
+            continue #Do not check holes
+        if i in n_link:
+            continue #Do not check links
+        if i in n_bounded_link:
+            continue #Do not check bounded links
         if ( substrates[i].xcor() == current_grid.xcor() ) and ( substrates[i].ycor() == (current_grid.ycor() + grid_length)):
             n.append(catalyst_index+grid_shift) # north of catalyst is occupied
         if ( substrates[i].xcor() == current_grid.xcor() ) and ( substrates[i].ycor() == (current_grid.ycor() - grid_length)):
@@ -161,7 +167,6 @@ def check_substrate_neighbors(n,current_grid,substrates,substrate_number,catalys
 
     return n
     
-                            #total_y_grid_number works as well since grids are square
 
 def delete_nonadjacent_neighbors(n,catalyst_index,total_x_grid_number):
     if catalyst_index - total_x_grid_number -1 in n: #south-west neighbor
@@ -189,13 +194,11 @@ def delete_nonadjacent_neighbors(n,catalyst_index,total_x_grid_number):
         if (catalyst_index+1 not in n) and (catalyst_index +total_x_grid_number not in n):
             n.remove(catalyst_index +total_x_grid_number +1)
 
-def create_link(n_subst,substrates):
+def create_link(n_link,n_hole,n_subst,substrates):
     # n contains indexes of grids that contain adjacent substrates
     # substrates held all of the substrates in the area    
     global global_iteration
     removed_subst = [] #either removed or turned to link
-    n_link = [] #hold link grid positions
-    n_holes = [] #hold holes grid positions
     i = 0
     while i < len(n_subst)-1:
         if ( n_subst[i+1] - n_subst[i]) == 1:
@@ -208,7 +211,7 @@ def create_link(n_subst,substrates):
                 i += 2
         else:
             i += 1                  
-        #input("Press Enter to see next instant")
+        input("Press Enter to see next instant")
         # Change iteration time and display it
         global_iteration += 1
         text_pen.clear()
@@ -237,7 +240,7 @@ def create_link(n_subst,substrates):
             i += 1
 
                   
-#        input("Press Enter to see next instant")
+        input("Press Enter to see next instant")
         # Change iteration time and display it
         global_iteration += 1
         text_pen.clear()
@@ -247,80 +250,124 @@ def create_link(n_subst,substrates):
         for i in range(len(removed_subst)):
             n_subst.remove(removed_subst[i])
 
-    return n_link, n_holes
 
 
-def check_link_neighbors(n_link,current_grid,substrates,grid_shift):
+def check_link_neighbors(n_link,substrates,grid_shift,current_ind):
     # This function checks whether current_grid's neighbors contain link and returns
     # index of neighbor grids that contain links
     n_bounded = []
+    current_grid = substrates[current_ind]
     for i in range(len(n_link)-1):
+        if i in n_holes:
+            continue
+        if i in n_substrates:
+            continue
+        if i in n_bounded_link:
+            continue
         if ( substrates[ n_link[i] ].xcor() == current_grid.xcor() ) and ( substrates[n_link[i]].ycor() == (current_grid.ycor() + grid_length)):
-            n_bounded.append(n_link[i]+grid_shift) # north of catalyst is occupied
+            n_bounded.append(current_ind+grid_shift) # north of catalyst is occupied
         if ( substrates[n_link[i]].xcor() == current_grid.xcor() ) and ( substrates[n_link[i]].ycor() == (current_grid.ycor() - grid_length)):
-            n_bounded.append(n_link[i]-grid_shift) # south of catalyst is occupied
+            n_bounded.append(current_ind-grid_shift) # south of catalyst is occupied
         if ( substrates[n_link[i]].xcor() == (current_grid.xcor() + grid_length) ) and (substrates[n_link[i]].ycor() == current_grid.ycor() ):
-            n_bounded.append(n_link[i] +1) # east of catalyst is occupied
+            n_bounded.append(current_ind +1) # east of catalyst is occupied
         if ( substrates[n_link[i]].xcor() == (current_grid.xcor() - grid_length) ) and (substrates[n_link[i]].ycor() == current_grid.ycor() ):
-            n_bounded.append(n_link[i] -1) # west of catalyst is occupied
+            n_bounded.append(current_ind -1) # west of catalyst is occupied
         if ( substrates[n_link[i]].xcor() == (current_grid.xcor() + grid_length) ) and ( substrates[n_link[i]].ycor() == (current_grid.ycor() + grid_length)):
-            n_bounded.append(n_link[i] + grid_shift +1) # north-east of catalyst is occupied
+            n_bounded.append(current_ind + grid_shift +1) # north-east of catalyst is occupied
         if ( substrates[n_link[i]].xcor() == (current_grid.xcor() - grid_length) ) and ( substrates[n_link[i]].ycor() == (current_grid.ycor() + grid_length)):
-            n_bounded.append(n_link[i] + grid_shift -1) # north-west catalyst is occupied
+            n_bounded.append(current_ind + grid_shift -1) # north-west catalyst is occupied
         if ( substrates[n_link[i]].xcor() == (current_grid.xcor() + grid_length) ) and ( substrates[n_link[i]].ycor() == (current_grid.ycor() - grid_length)):
-            n_bounded.append(n_link[i]-grid_shift + 1) # south-east catalyst is occupied
+            n_bounded.append(current_ind-grid_shift + 1) # south-east catalyst is occupied
         if ( substrates[n_link[i]].xcor() == (current_grid.xcor() - grid_length) ) and ( substrates[n_link[i]].ycor() == (current_grid.ycor() - grid_length)):
-            n_bounded.append(n_link[i]-grid_shift -1) # south-west of catalyst is occupied
+            n_bounded.append(current_ind-grid_shift -1) # south-west of catalyst is occupied
 
     return n_bounded      
 
 
 
 def create_bounded_link(current_link_index,neighbor_links,substrates):
-    n_bounded = []
+    n_bound = []
     substrates[current_link_index].color("blue")
-    n_bounded.append(current_link_index)
+    n_bound.append(current_link_index)
     for i in range(len(neighbor_links)):
         substrates[neighbor_links[i]].color("blue")
-        n_bounded.append(neighbor_links[i]) 
-    return n_bounded
+        n_bound.append(neighbor_links[i]) 
+    return n_bound
+
+def check_hole_neighbors(n_holes,current_grid,substrates,grid_shift):
+    # This function checks whether current_grid's neighbors contain holes and returns
+    # index of neighbor grids that contain holes  
+    n_hole_neighbors = []
+    for i in range(len(n_holes)):
+        if ( substrates[n_holes[i] ].xcor() == current_grid.xcor() ) and ( substrates[n_holes[i]].ycor() == (current_grid.ycor() + grid_length)):
+            n_hole_neighbors.append(catalyst_index+grid_shift) # north of catalyst is occupied
+        if ( substrates[n_holes[i] ].xcor() == current_grid.xcor() ) and ( substrates[n_holes[i]].ycor() == (current_grid.ycor() - grid_length)):
+            n_hole_neighbors.append(catalyst_index-grid_shift) # south of catalyst is occupied
+        if ( substrates[n_holes[i] ].xcor() == (current_grid.xcor() + grid_length) ) and (substrates[n_holes[i]].ycor() == current_grid.ycor() ):
+            n_hole_neighbors.append(catalyst_index +1) # east of catalyst is occupied
+        if ( substrates[n_holes[i] ].xcor() == (current_grid.xcor() - grid_length) ) and (substrates[n_holes[i]].ycor() == current_grid.ycor() ):
+            n_hole_neighbors.append(catalyst_index -1) # west of catalyst is occupied
+        if ( substrates[n_holes[i] ].xcor() == (current_grid.xcor() + grid_length) ) and ( substrates[n_holes[i]].ycor() == (current_grid.ycor() + grid_length)):
+            n_hole_neighbors.append(catalyst_index + grid_shift +1) # north-east of catalyst is occupied
+        if ( substrates[n_holes[i] ].xcor() == (current_grid.xcor() - grid_length) ) and ( substrates[n_holes[i]].ycor() == (current_grid.ycor() + grid_length)):
+            n_hole_neighbors.append(catalyst_index + grid_shift -1) # north-west catalyst is occupied
+        if ( substrates[n_holes[i] ].xcor() == (current_grid.xcor() + grid_length) ) and ( substrates[n_holes[i]].ycor() == (current_grid.ycor() - grid_length)):
+            n_hole_neighbors.append(catalyst_index-grid_shift + 1) # south-east catalyst is occupied
+        if ( substrates[n_holes[i] ].xcor() == (current_grid.xcor() - grid_length) ) and ( substrates[n_holes[i]].ycor() == (current_grid.ycor() - grid_length)):
+            n_hole_neighbors.append(catalyst_index-grid_shift -1) # south-west of catalyst is occupied
+
+    return n_hole_neighbors
+
+def move_catalyst(desired_grid,substrates):
+    # Instead of mapping grid number to exact position, I use small trick.
+    # I am using using substrates to get exact location info
+    x_desired = substrates[desired_grid].position()[0]
+    y_desired = substrates[desired_grid].position()[1]
+    catalyst.goto(x_desired,y_desired)
+    global global_iteration
+    global_iteration += 1
+    text_pen.clear()
+    display_global_time(grid_length,area_length)
+
+
 #####################################################
 
 #####################################################
 ## Production (Number 4 in Paper Appendix)    
-# Find neighbor substrates to catalyst
-n1 = [] # occupied neighbor's index
-n_substrates = check_substrate_neighbors(n1,catalyst,substrates,substrate_number,catalyst_index,area_length//grid_length)
-delete_nonadjacent_neighbors(n_substrates,catalyst_index, area_length//grid_length)
+n_substrates = [] # occupied neighbor's index
+n_holes = []
+n_link = []
+n_bounded_link = []
 
-(n_link, n_holes) = create_link(n_substrates,substrates)
-n_link_neighbors = []
-for i in range(len(n_link)):
-    current_link_ind = n_link[i]
-    del n_link[i] #To not check the grid with itself
-    n_link_neighbors = check_link_neighbors(n_link,substrates[current_link_ind],substrates, area_length//grid_length)
-    if len(n_link_neighbors) != 0: #there are neighbor links 
-        n_bounded_link = create_bounded_link(current_link_ind,n_link_neighbors,substrates)
-    n_link.insert(i,current_link_ind)
+try:
+    while True:
+        n_substrates = check_substrate_neighbors(catalyst,substrates,substrate_number,catalyst_index,area_length//grid_length,n_holes,n_link,n_bounded_link)
+        delete_nonadjacent_neighbors(n_substrates,catalyst_index, area_length//grid_length)
+
+        create_link(n_link,n_holes,n_substrates,substrates)
+        n_link_neighbors = []
+        for i in range(len(n_link)):
+            current_link_ind = n_link[i]
+            del n_link[i] #To not check the grid with itself
+            n_link_neighbors = check_link_neighbors(n_link,substrates, area_length//grid_length,current_link_ind)
+            if len(n_link_neighbors) != 0: #there are neighbor links 
+                n_bounded_link = create_bounded_link(current_link_ind,n_link_neighbors,substrates)
+            n_link.insert(i,current_link_ind)
+        
+        n_hole_neighbors = check_hole_neighbors(n_holes,catalyst,substrates,area_length//grid_length)
+        if len(n_hole_neighbors) != 0:
+            n_holes.append(catalyst_index)
+            catalyst_index = random.choice(n_hole_neighbors)
+            if catalyst_index in n_holes:
+                n_holes.remove(catalyst_index)
+            input("Press Enter to see next instant")
+            move_catalyst(catalyst_index,substrates)
+
+except KeyboardInterrupt:
+    pass
 
 
 
-
-
-
-
-
-#for i in range(substrate_number):
-    # if ( substrates[i].xcor() == catalyst.xcor() ) and ( substrates[i].ycor() == (catalyst.ycor() + grid_length)):
-    #     n.append(i) # north of catalyst is occupied
-    # if ( substrates[i].xcor() == catalyst.xcor() ) and ( substrates[i].ycor() == (catalyst.ycor() - grid_length)):
-    #     n.append(i) # south of catalyst is occupied
-    # if ( substrates[i].xcor() == (catalyst.xcor() + grid_length) ) and (substrates[i].ycor() == catalyst.ycor() ):
-    #     n.append(i) # east of catalyst is occupied
-    # if ( substrates[i].xcor() == (catalyst.xcor() - grid_length) ) and (substrates[i].ycor() == catalyst.ycor() ):
-    #     n.append(i) # west of catalyst is occupied
-
-print("Oh yeah")
 
 
 
